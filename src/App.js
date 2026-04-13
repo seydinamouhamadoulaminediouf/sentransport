1c39f4ab58d71adc430e53079a02a0c4c0277442
Seydina Mouhamadou Lamine Diouf <seydinamouhamadoulaminediouf@esp.sn>
2026-04-13 12:24:47 +0000

    Setup initial SenTransport avec Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <main className="contenu">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue ! Cette application vous aide a trouver votre ligne de bus a
+          Dakar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </main>
     </div>
   );
 }
-
 export default App;
