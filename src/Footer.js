c3853bbe38c39a2f64a8dfeed9c630a530ac54bb
Seydina Mouhamadou Lamine Diouf <seydinamouhamadoulaminediouf@esp.sn>
2026-05-02 21:48:04 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2026 SenTransport &mdash; ESP/UCAD</p>
+    </footer>
+  );
+}
+export default Footer;
